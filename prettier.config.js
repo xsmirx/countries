be0e2fc2b9b2868c71d88d285d7651c7a8c1c91d
Ivan S <xsmirx@gmail.com>
2023-06-05 20:44:09 +0400

    initial shadcn/ui and prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  jsxSingleQuote: false,
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
