a86a65d2c70688ad5cbef2ab643d8f490b2277fc
Ivan S <xsmirx@gmail.com>
2023-06-09 22:10:08 +0400

    fix: edit layouts

--- app/[cca3]/page.tsx
+++ app/[cca3]/page.tsx
@@ -2,6 +2,7 @@ import {CountryResp} from '@/types/countries'
 import {TypographyH1} from '@/components/ui/typography'
 import Image from 'next/image'
 import {Badge} from '@/components/ui/badge'
+import {AspectRatio} from '@/components/ui/aspect-ratio'
 
 type Props = {
   params: {cca3: string}
@@ -21,7 +22,7 @@ export default async function ({params: {cca3}}: Props) {
         {country.name.official}
       </TypographyH1>
       <div className="flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-10">
-        <div className="relative w-full py-[33.5%] sm:h-[200px] sm:w-[300px] sm:p-0">
+        <div className="relative w-full flex-1 py-[33.5%] sm:h-[200px] sm:w-[300px] sm:p-0">
           <Image
             src={country.flags.svg}
             alt={country.cca3 + ' flag'}
@@ -29,9 +30,9 @@ export default async function ({params: {cca3}}: Props) {
             className="object-contain object-center"
           />
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-1 flex-col gap-4">
           <div>
-            <span className="font-semibold">Native Name: </span>
+            <p className="font-semibold">Native Name: </p>
             <p className="inline-flex flex-col">
               {languages.map(language => (
                 <span key={language}>
@@ -41,44 +42,48 @@ export default async function ({params: {cca3}}: Props) {
               ))}
             </p>
           </div>
-          <p>
-            <span className="font-semibold">Population: </span>
-            {country.population.toLocaleString('en-US')}
-          </p>
-          <p>
-            <span className="font-semibold">Region: </span>
-            {country.region}
-          </p>
-          <p>
-            <span className="font-semibold">Sub Region: </span>
-            {country.subregion}
-          </p>
-          <p>
-            <span className="font-semibold">Capital: </span>
-            {country.capital}
-          </p>
-          <p>
-            <span className="font-semibold">Top Lavel Domain: </span>
-            {country.tld}
-          </p>
           <div>
-            <span className="font-semibold">Currencies: </span>
-            <p className="inline-flex gap-1">
+            <p className="font-semibold">Population: </p>
+            {country.population.toLocaleString('en-US')}
+          </div>
+          <div>
+            <p className="font-semibold">Region: </p>
+            {country.region}
+          </div>
+          <div>
+            <p className="font-semibold">Sub Region: </p>
+            {country.subregion}
+          </div>
+          <div>
+            <p className="font-semibold">Capital: </p>
+            {country.capital}
+          </div>
+          <div>
+            <p className="font-semibold">Top Lavel Domain: </p>
+            <div className="inline-flex flex-wrap gap-1">
+              {country.tld.map(tld => (
+                <span key={tld}>{tld}</span>
+              ))}
+            </div>
+          </div>
+          <div>
+            <p className="font-semibold">Currencies: </p>
+            <div className="inline-flex gap-1">
               {currencies.map(currency => (
                 <span key={currency}>{currency}</span>
               ))}
-            </p>
+            </div>
           </div>
           <div>
-            <span className="font-semibold">Languages: </span>
-            <p className="inline-flex gap-1">
+            <p className="font-semibold">Languages: </p>
+            <div className="inline-flex gap-1">
               {languages.map(language => (
                 <span key={language}>{language}</span>
               ))}
-            </p>
+            </div>
           </div>
           <div>
-            <span className="font-semibold">Border Countries: </span>
+            <p className="font-semibold">Border Countries: </p>
             <div className="inline-flex flex-wrap gap-1">
               {country.borders.map(country => (
                 <Badge>{country}</Badge>
